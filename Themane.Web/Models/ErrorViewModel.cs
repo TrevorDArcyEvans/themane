6601dd9a40ff15395773a45f8bd50655d5f23edd
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-12-19 15:23:49 +0000

    Add project files.

---
+++ Themane.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Themane.Web.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
