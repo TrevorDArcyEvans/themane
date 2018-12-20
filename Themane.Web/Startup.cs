320a492a2318fbc106c4592cbec5cb559b794dc2
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-12-20 14:02:58 +0000

    - cleanup default route

--- Themane.Web/Startup.cs
+++ Themane.Web/Startup.cs
@@ -51,9 +51,7 @@ namespace Themane.Web
 
       app.UseMvc(routes =>
       {
-        routes.MapRoute(
-                  name: "default",
-                  template: "{controller=Home}/{action=Index}/{id?}");
+        routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}");
       });
     }
   }
