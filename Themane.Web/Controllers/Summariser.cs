dfcee49ad12c4bf68f8fd9f68486f30504d16931
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-01-03 09:59:25 +0000

    - moved summarisers to separate class

---
+++ Themane.Web/Controllers/Summariser.cs
@@ -0,0 +1,60 @@
+﻿using OpenTextSummarizer;
+using System.Diagnostics;
+using System.Text;
+using TextRank;
+using CP = CodePlex.OpenTextSummarizer;
+
+namespace Themane.Web.Controllers
+{
+  internal static class Summariser
+  {
+    public static string TextRank(string inputText, int wordLength)
+    {
+      var sw = Stopwatch.StartNew();
+      var extract = new ExtractKeyPhrases().Extract(inputText, wordLength);
+      var sb = new StringBuilder();
+      sb.AppendLine($"Summarised content in {sw.ElapsedMilliseconds} ms");
+      sb.AppendLine(" ===== Summary =============================== ");
+      sb.AppendLine(extract.Item1);
+      sb.AppendLine(" ===== Keywords =============================== ");
+      extract.Item2?.ForEach(kw => sb.AppendLine(kw));
+      return sb.ToString();
+    }
+
+    public static string OpenText(string inputText, int maxSummSentences)
+    {
+      var sw = Stopwatch.StartNew();
+      var args = new SummarizerArguments
+      {
+        MaxSummarySentences = maxSummSentences,
+        MaxSummarySizeInPercent = 100
+      };
+      var summarizedDocument = Summarizer.Summarize(new DirectTextContentProvider(inputText), args);
+      var sb = new StringBuilder();
+      sb.AppendLine($"Summarised content in {sw.ElapsedMilliseconds} ms");
+      sb.AppendLine(" ===== Summary =============================== ");
+      summarizedDocument.Sentences.ForEach(s => sb.AppendLine(string.Format("{0}\r\n", s)));
+      sb.AppendLine(" ===== Keywords =============================== ");
+      summarizedDocument.Concepts.ForEach(c => sb.AppendLine(string.Format("\t{0}", c)));
+      return sb.ToString();
+    }
+
+    public static string CodePlexOpenText(string inputText, int displayPercent)
+    {
+      var sw = Stopwatch.StartNew();
+      var args = new CP.OpenTextSummarizer.SummarizerArguments
+      {
+        DisplayPercent = displayPercent,
+        InputString = inputText
+      };
+      var summarizedDocument = CP.OpenTextSummarizer.Summarizer.Summarize(args);
+      var sb = new StringBuilder();
+      sb.AppendLine($"Summarised content in {sw.ElapsedMilliseconds} ms");
+      sb.AppendLine(" ===== Summary =============================== ");
+      summarizedDocument.Sentences.ForEach(s => sb.AppendLine(string.Format("{0}\r\n", s)));
+      sb.AppendLine(" ===== Keywords =============================== ");
+      summarizedDocument.Concepts.ForEach(c => sb.AppendLine(string.Format("\t{0}", c)));
+      return sb.ToString();
+    }
+  }
+}
--- Themane.Web/Controllers/TextSummaryController.cs
+++ Themane.Web/Controllers/TextSummaryController.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using OpenTextSummarizer;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
 using System.Threading.Tasks;
-using TextRank;
 using Themane.Web.Models;
-using CP = CodePlex.OpenTextSummarizer;
 
 namespace Themane.Web.Controllers
 {
@@ -22,9 +17,9 @@ namespace Themane.Web.Controllers
     [Authorize]
     public IActionResult SummaryResults(TextSummary model)
     {
-      var txt1 = Task<string>.Factory.StartNew(() => Summarise_TextRank(model.InputText, model.TextRank_MaxWords));
-      var txt2 = Task<string>.Factory.StartNew(() => Summarise_OpenText(model.InputText, model.OpenText_MaxSentences));
-      var txt3 = Task<string>.Factory.StartNew(() => Summarise_CodePlexOpenText(model.InputText, model.CodePlexOpenText_DisplayPercent));
+      var txt1 = Task<string>.Factory.StartNew(() => Summariser.TextRank(model.InputText, model.TextRank_MaxWords));
+      var txt2 = Task<string>.Factory.StartNew(() => Summariser.OpenText(model.InputText, model.OpenText_MaxSentences));
+      var txt3 = Task<string>.Factory.StartNew(() => Summariser.CodePlexOpenText(model.InputText, model.CodePlexOpenText_DisplayPercent));
       var tasks = new List<Task>(new[] { txt1, txt2, txt3 });
       Task.WaitAll(tasks.ToArray());
 
@@ -37,55 +32,6 @@ namespace Themane.Web.Controllers
       };
 
       return View(result);
-    }
-
-    private static string Summarise_TextRank(string inputText, int wordLength)
-    {
-      var sw = Stopwatch.StartNew();
-      var extract = new ExtractKeyPhrases().Extract(inputText, wordLength);
-      var sb = new StringBuilder();
-      sb.AppendLine($"Summarised content in {sw.ElapsedMilliseconds} ms");
-      sb.AppendLine(" ===== Summary =============================== ");
-      sb.AppendLine(extract.Item1);
-      sb.AppendLine(" ===== Keywords =============================== ");
-      extract.Item2?.ForEach(kw => sb.AppendLine(kw));
-      return sb.ToString();
-    }
-
-    private static string Summarise_OpenText(string inputText, int maxSummSentences)
-    {
-      var sw = Stopwatch.StartNew();
-      var args = new SummarizerArguments
-      {
-        MaxSummarySentences = maxSummSentences,
-        MaxSummarySizeInPercent = 100
-      };
-      var summarizedDocument = Summarizer.Summarize(new DirectTextContentProvider(inputText), args);
-      var sb = new StringBuilder();
-      sb.AppendLine($"Summarised content in {sw.ElapsedMilliseconds} ms");
-      sb.AppendLine(" ===== Summary =============================== ");
-      summarizedDocument.Sentences.ForEach(s => sb.AppendLine(string.Format("{0}\r\n", s)));
-      sb.AppendLine(" ===== Keywords =============================== ");
-      summarizedDocument.Concepts.ForEach(c => sb.AppendLine(string.Format("\t{0}", c)));
-      return sb.ToString();
-    }
-
-    private static string Summarise_CodePlexOpenText(string inputText, int displayPercent)
-    {
-      var sw = Stopwatch.StartNew();
-      var args = new CP.OpenTextSummarizer.SummarizerArguments
-      {
-        DisplayPercent = displayPercent,
-        InputString = inputText
-      };
-      var summarizedDocument = CP.OpenTextSummarizer.Summarizer.Summarize(args);
-      var sb = new StringBuilder();
-      sb.AppendLine($"Summarised content in {sw.ElapsedMilliseconds} ms");
-      sb.AppendLine(" ===== Summary =============================== ");
-      summarizedDocument.Sentences.ForEach(s => sb.AppendLine(string.Format("{0}\r\n", s)));
-      sb.AppendLine(" ===== Keywords =============================== ");
-      summarizedDocument.Concepts.ForEach(c => sb.AppendLine(string.Format("\t{0}", c)));
-      return sb.ToString();
     }
   }
 }
