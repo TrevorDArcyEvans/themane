1c6bb7ace5d08ccecbef1468d2651a746e25bc94
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-01-07 10:17:34 +0000

    - documentation

--- Themane.Web/Models/Contact.cs
+++ Themane.Web/Models/Contact.cs
@@ -7,7 +7,7 @@
     public string GivenName { get; set; } = string.Empty;
     public string FullName { get { return $"{GivenName} {Surname}"; } }
     public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;  // hash
     public string Role { get; set; } = string.Empty;
     public string CompanyId { get; set; }
   }
