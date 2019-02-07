9d1797ea1a8f226dbeb597b4dcd3aab244741d10
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-02-07 08:38:25 +0000

    - SQLite compatibility

--- Data/SeedDatabase.MSSQLServer.sql
+++ Data/SeedDatabase.MSSQLServer.sql
@@ -2,8 +2,8 @@ INSERT INTO Company (Id, Name) VALUES
 ('Company02', 'Company02'),
 ('Company01', 'Company01'),
 ('Company03', 'Company03'),
-('themane'  , 'themane')
-GO
+('themane'  , 'themane');
+
 
 -- MD5 hashes from:
 --  https://passwordsgenerator.net/md5-hash-generator/
@@ -15,16 +15,16 @@ INSERT INTO Contact (Id, CompanyId, GivenName, Surname, Email, Password, Role) V
 ('trevor'   , 'themane'  , 'Trevor', 'DArcy-Evans' , 'trevor@themane.com'    , 'D801A0B4701F64A0EAE5F71F44F8D83F', 'Admin' ), -- trevor
 ('helen'    , 'themane'  , 'Helen' , 'Hey'         , 'helen@themane.com'     , '7A2EB41A38A8F4E39C1586649DA21E5F', 'Admin' ), -- helen
 ('mandy'    , 'themane'  , 'Mandy' , 'DArcy-Evans' , 'mandy@themane.com'     , '006CE4922072B1E7F8B733347FE1A40B', 'Admin' ), -- mandy
-('neil'     , 'themane'  , 'Neil'  , 'Hey'         , 'neil@themane.com'      , 'D68E939882371200637D5024B360FC20', 'Admin' )  -- neil
- GO
+('neil'     , 'themane'  , 'Neil'  , 'Hey'         , 'neil@themane.com'      , 'D68E939882371200637D5024B360FC20', 'Admin' ); -- neil
+ 
 
  INSERT INTO Usage(Id, ContactId, DateTimeUTC, InputText) VALUES
 ('Usage01', 'trevor', '19650715 10:34:09 AM', 'inputtext01'),
 ('Usage02', 'helen' , '20120618 10:34:09 AM', 'inputtext02'),
 ('Usage03', 'mandy' , '19631019 12:35:00 AM', 'inputtext03'),
 ('Usage04', 'neil'  , '20060220 06:03:00 AM', 'inputtext04'),
-('Usage05', 'neil'  , '20190107 02:20:00 PM', 'inputtext05')
-GO
+('Usage05', 'neil'  , '20190107 02:20:00 PM', 'inputtext05');
+
 
 
 
